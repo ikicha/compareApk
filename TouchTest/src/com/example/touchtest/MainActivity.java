5975ad4108d2563670ace9be1baf4444f2eacaad
Jeongik Cha <ikicha90@ubuntu.(none)>
2014-07-06 13:03:17 +0900

    Project init

---
+++ TouchTest/src/com/example/touchtest/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.touchtest;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	public boolean onTouchEvent(MotionEvent event) {
+		int action = event.getAction();
+		switch (action & MotionEvent.ACTION_MASK) {
+		case MotionEvent.ACTION_POINTER_DOWN:
+			// multitouch!! - touch down
+			int count = event.getPointerCount(); // Number of 'fingers' in this
+													// time
+			break;
+		}
+		return false;
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main, container,
+					false);
+			return rootView;
+		}
+	}
+
+}
---
+++ com/ikicha/compareApk.py
@@ -0,0 +1,66 @@
+'''
+Created on 2014. 7. 5.
+
+@author: jeongik.cha
+'''
+
+import zipfile
+import os
+import sys
+
+num_argv = len(sys.argv)
+argv = sys.argv
+
+if num_argv<3:
+    print "ERROR : Wrong Arguments Type"
+    print "compareApk.pyc [APK FILE NAME] [RES FOLDER IN PROJECT] ... ( RES FOLDER CAN BE ONE OR MORE )"
+    exit()
+
+if num_argv>=2:
+    myZip = zipfile.ZipFile(argv[1])
+else:
+    myZip = zipfile.ZipFile("../../TouchTest/bin/TouchTest.apk")
+     
+apkFileNames = myZip.namelist()
+resFileFromApk = []
+for name in filter(lambda s:'res/' in s and 'drawable' in s, apkFileNames):
+    resFileFromApk.append(str(name.split("res/")[1]))
+
+resFileFromProject = []
+
+if num_argv>=3:
+    roots = argv[2:]
+else:
+    roots = ["../../TouchTest/res", "../../res"]
+    
+for root in roots:
+    for base, dirs, names in os.walk(root):
+        if("res"+os.path.sep in base and "drawable" in base):
+            for name in names:
+                if name != "Thumbs.db":
+                    resFileFromProject.append(str(os.path.join(base.split("res"+os.path.sep)[1], name)).replace('\\', '/'))
+            
+resFileFromApk.sort()            
+resFileFromProject.sort()
+
+sizeApk = len(resFileFromApk)
+sizeProject = len(resFileFromProject)
+
+print str(sizeApk) +" res files in Apk"
+print str(sizeProject) + " res files in Project"
+'''print resFileFromApk
+print resFileFromProject'''
+ever_miss = 0
+for fileInProject in resFileFromProject:
+    hit = False
+    for fileInApk in resFileFromApk:
+        if(fileInApk == fileInProject):
+            hit = True
+    if not hit:
+        print fileInProject + " is not in Apk"
+        ever_miss = ever_miss + 1
+if ever_miss == 0:
+    print "PASS"
+else:  
+    print "FAIL"
+    print str(ever_miss) + " files is missing in Apk."
